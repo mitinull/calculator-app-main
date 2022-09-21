220c98ce0fe44e2541d647477edbcdceb8273eb8
mohammadT9030 <torabi9030@gmail.com>
2022-09-21 15:06:05 +0430

    implement UI and Themes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const themes = ["theme1", "theme2", "theme3"];
+const margins = ["5px", "26px", "50px"];
+const btn = document.querySelector(".header .button");
+let index = 0;
+document.querySelector(".button-container").addEventListener("click", () => {
+  index = (index + 1) % themes.length;
+  document.documentElement.className = themes[index];
+  btn.style.marginLeft = margins[index];
+});
